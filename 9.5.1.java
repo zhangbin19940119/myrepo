ae231012b1c9e15a77a8be805a948d6d9bb565d3
zhangbin19940119 <1147216899@qq.com>
2016-09-05 16:40:40 +0800

    haha

---
+++ 9.5.1.java
@@ -0,0 +1,10 @@
+import javax.swing.*,
+class Greetings{
+	public static void main(String[]args){
+		
+		String fullName;
+		fullName=JOptionpane.showInputDialog(null,请问输入你的名字：");
+		JOptionPane.showMessageDialog(null,"欢迎:"+fullName+"开始梦幻的java旅程!");
+		System.exit(0);
+	}	
+}
